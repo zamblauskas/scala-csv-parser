31fb01891bafc5b5e53266690e3acb0d108d3fca
Robertas Zamblauskas <robertas.zamblauskas@gmail.com>
2017-01-11 10:45:23 +0200

    Added test for maximum params (22) case class parsing.

--- src/test/scala/zamblauskas/CsvParserSpec.scala
+++ src/test/scala/zamblauskas/CsvParserSpec.scala
@@ -128,6 +128,23 @@ class CsvParserSpec extends FunSpec with Matchers {
     }
   }
 
+  describe("maximum size case class") {
+    it("parse 22 params case class") {
+      case class Maximum(p1: Int, p2: Int, p3: Int, p4: Int, p5: Int, p6: Int, p7: Int, p8: Int, p9: Int, p10: Int,
+        p11: Int, p12: Int, p13: Int, p14: Int, p15: Int, p16: Int, p17: Int, p18: Int, p19: Int, p20: Int, p21: Int, p22: Int)
+
+      val csv =
+        """
+        |p1,p2,p3,p4,p5,p6,p7,p8,p9,p10,p11,p12,p13,p14,p15,p16,p17,p18,p19,p20,p21,p22
+        |1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22
+      """.stripMargin
+
+      parse[Maximum](csv) shouldBe Right(Seq(
+        Maximum(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22)
+      ))
+    }
+  }
+
   def testReads(implicit cr: ColumnReads[Person]): Unit = {
     describe("success parsing") {
       it("empty string") {
